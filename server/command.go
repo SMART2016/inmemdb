cd42a94b7deb7c7e787ed193efdd8dd60963d5af
dmazumder <dmazumder@hhaexchange.com>
2023-09-28 11:47:59 +0530

    ADDED command constant

--- server/command.go
+++ server/command.go
@@ -9,6 +9,12 @@ import (
 Commands sent to the server are generally an operation followed by a set of arguments
 Example: PUT Key Value
 */
+
+const (
+	COMMAND_PING          = "ping"
+	COMMAND_PING_RESPONSE = "PONG"
+)
+
 type Command struct {
 	//The operation to perform
 	Cmd string
@@ -20,7 +26,7 @@ type Command struct {
 func (cmd *Command) EvalCommand() ([]byte, error) {
 	log.Println("comamnd:", cmd.Cmd)
 	switch cmd.Cmd {
-	case "PING":
+	case COMMAND_PING:
 		return cmd.evalPING()
 	default:
 		return cmd.evalPING()
@@ -35,7 +41,7 @@ func (cmd *Command) evalPING() ([]byte, error) {
 	}
 
 	if len(cmd.Args) == 0 {
-		b = Encode("PONG", true)
+		b = Encode(COMMAND_PING_RESPONSE, true)
 	} else {
 		b = Encode(cmd.Args[0], false)
 	}
